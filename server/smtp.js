bd1afbdb82d2f85dbfe9d068c5407cc6a253ef3b
Tuan Hoang <anhtuan.hoangvu@gmail.com>
2018-05-27 11:34:38 +0700

    - Minor bug fix - Add balance for DCFund - Email test

--- server/smtp.js
+++ server/smtp.js
@@ -1,3 +1,3 @@
 Meteor.startup(function () {
-    process.env.MAIL_URL = 'smtp://apikey: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@smtp.sendgrid.net:587';
+    process.env.MAIL_URL = 'smtp://apikey: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@smtp.sendgrid.net:465';
 });
